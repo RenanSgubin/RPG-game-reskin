195a459e5dff608e44b0736bb36437e52541dee3
= <renan08042004@gmail.com>
2022-09-02 23:20:15 -0300

    -Adicionado os arquivos via upload
    -Continuado o desenvolvimento de tela de status do personagem (botão de fechar, texto "stats" no topo, aba de aumentar status e imagem da skin)

--- js/showSkillDescriptions.js
+++
@@ -1,9 +0,0 @@
-
-
-function showSkillDescription( classNumber ) {
-    document.getElementsByClassName("skill-description")[classNumber].style.display = "block";
-}
-
-function displayNoneSkillDescription( classNumber ) {
-    document.getElementsByClassName("skill-description")[classNumber].style.display = "none";
-}
