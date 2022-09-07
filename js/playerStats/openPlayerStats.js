9b4d57be5591cbfb79e11818eba330a8f0ed1b7b
= <renan08042004@gmail.com>
2022-09-06 21:48:36 -0300

    -Adicionado os arquivos via upload
    -Alterado o estilo das sprites dos inimigos e do personagem
    -Adicionado barra de xp à aba de status do personagem
    -Feito a sprite inicial do Knight
    -Começado o sistema de equipar itens e mudar a skin do personagem

---
+++ js/playerStats/openPlayerStats.js
@@ -0,0 +1,7 @@
+function openPlayerStats() {
+    document.getElementsByClassName("player-stats-container ")[0].style.top = "50%";
+}
+
+function closePlayerStats() {
+    document.getElementsByClassName("player-stats-container")[0].style.top = "-150%";
+}
